c2afc4df87000ca3877cdd15bb54a33f9e44e93e
ryanhighsmith09 <ryanhighsmith09@gmail.com>
2026-02-06 21:14:09 -0600

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,23 @@
+Config = {}
+
+Config.Tables = {
+    {
+        coords = vector3(1995.2, 3049.3, 47.2),
+        heading = 90.0,
+        model = `prop_pooltable_02`
+    },
+    {
+        coords = vector3(-561.1, 286.5, 82.2),
+        heading = 180.0,
+        model = `prop_pooltable_02`
+    }
+}
+
+Config.BallModels = {
+    cue = `prop_poolball_cue`,
+    solids = `prop_poolball_1`,
+    stripes = `prop_poolball_9`,
+    eight = `prop_poolball_8`
+}
+
+Config.CueProp = `prop_pool_cue`
