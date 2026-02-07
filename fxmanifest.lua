4cbea27b17fb02edf5563dc6b46363c44b8cc272
ryanhighsmith09 <ryanhighsmith09@gmail.com>
2026-02-06 21:13:15 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'YourName'
+description 'Standalone Multiplayer Pool (8-Ball)'
+version '1.0.0'
+
+shared_script 'config.lua'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+ui_page 'html/ui.html'
+
+files {
+    'html/ui.html'
+}
 
